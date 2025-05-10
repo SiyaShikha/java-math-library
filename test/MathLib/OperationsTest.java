a5d11b0892a45393ddf0615d46e27ee7b80259fc
Shikha <150659967+SiyaShikha@users.noreply.github.com>
2025-05-10 16:12:47 +0530

    created a MathLib package Library

---
+++ test/MathLib/OperationsTest.java
@@ -0,0 +1,24 @@
+package MathLib;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperationsTest {
+    @Test
+    void name() {
+        assertEquals(1,1);
+    }
+
+    @Test
+    void add() {
+//        Operations operations = new Operations();
+        assertEquals(3, Operations.add(1,2));
+    }
+
+    @Test
+    void sub() {
+//        Operations operations = new Operations();
+        assertEquals(1,Operations.sub(2,1));
+    }
+}
